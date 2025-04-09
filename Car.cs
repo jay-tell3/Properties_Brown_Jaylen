60914702624ba5e29e35d37f423d45164e76bd73
jay-tell3 <jaybroww@gmail.com>
2025-04-09 14:25:08 -0500

    bruh

---
+++ Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties_Brown_Jaylen
+{
+    internal class Car
+    {
+        //makes a string varable
+        private string model;
+        //Makes a propertie for the model
+        public string Model { get { return model; } set { model = value; } }
+        //makes an automatice propertie for the make
+        public string Make { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace Properties_Brown_Jaylen
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Properties_Brown_Jaylen
 {
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {   //makes a new game obj from the car class
+            Car car = new Car();
+            //sets the make of the frist car
+            car.Make = "ford";
+            //sets the model of the frist car
+            car.Model = "mustang";
+            //Tells the user make and model of the first car
+            Console.WriteLine($"Make: {car.Make} \nModle: {car.Model}\n");
+            //makes a new game obj from the car class
+            Car car2 = new Car();
+            //sets the make of the second car
+            car2.Make = "Chevy";
+            //sets the model of the second car
+            car2.Model = "Camero";
+            //Tells the user make and model of the second car
+            Console.WriteLine($"Make: {car2.Make} \nModle: {car2.Model}");
         }
     }
 }
